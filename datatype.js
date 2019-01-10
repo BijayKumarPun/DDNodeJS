d523a8788f0bdcd72a08e415e9aae00852d1c561
homay <bijay.workplace@gmail.com>
2019-01-10 15:09:52 +0545

    data type

---
+++ datatype.js
@@ -0,0 +1,27 @@
+console.log('this is data type example');
+
+
+var x = 20;
+var y  = '20';
+var z;
+console.log(x+x);
+console.log(y+y);
+console.log(z);
+
+//null values
+z = null;
+console.log(z);
+
+// use of typeof operator
+//it shows the type of anything
+
+console.log(typeof 50); //number
+console.log(typeof 'ram'); //string
+console.log(typeof function(){ console.log('inside some function')}); //function
+
+//variable declaration
+//use uppercase letters for constants
+
+var MY_CONSTATNT = 40;
+
+console.log(MY_CONSTATNT);
